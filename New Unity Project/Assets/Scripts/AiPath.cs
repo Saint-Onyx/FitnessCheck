4002eb5cfecbc914471a0526b2c1e0f0fc8c682c
Daniel Jazz Young <danny.young@icaros.com>
2019-08-23 14:22:31 +0200

    ...

---
+++ New Unity Project/Assets/Scripts/AiPath.cs
@@ -0,0 +1,196 @@
+﻿//using System;
+//using System.Xml.Serialization;
+//using static SteroidsSharedData.Helpers.Constants;
+
+//namespace SteroidsSharedData.Serialization
+//{
+//    /// <summary>
+//    /// Class used for storing a path used for ai instances, and then is later deserialized on the server side
+//    /// </summary>
+//    [Serializable()]
+//    [XmlRoot(nameof(AiPath))]
+//    public class AiPath
+//    {
+//        /// <summary>
+//        /// Collection holding path nodes
+//        /// </summary>
+//        [XmlArray(nameof(PathNodeCollection))]
+//        [XmlArrayItem(nameof(AiPathNode), typeof(AiPathNode))]
+//        public AiPathNode[] PathNodeCollection { get; set; }
+
+//        /// <summary>
+//        /// Name of the level
+//        /// </summary>
+//        [XmlElement(nameof(LevelName))]
+//        public AvailableLevels LevelName { get; set; }
+
+//        /// <summary>
+//        /// Finish index of this path (finish line crossing moment)
+//        /// </summary>
+//        [XmlElement(nameof(BookedLaps))]
+//        public int BookedLaps { get; set; }
+
+//        /// <summary>
+//        /// Index for the Startposition
+//        /// </summary>
+//        [XmlElement(nameof(StartPositionIndex))]
+//        public int StartPositionIndex { get; set; }
+
+//        /// <summary>
+//        /// FinishTime of this path (finish line crossing moment)
+//        /// </summary>
+//        [XmlElement(nameof(TotalTime))]
+//        public double TotalTime { get; set; }
+
+//        /// <summary>
+//        /// BestTime of this path (finish line crossing moment)
+//        /// </summary>
+//        [XmlElement(nameof(BestTime))]
+//        public double BestTime { get; set; }
+//    }
+
+//    /// <summary>
+//    /// An element of the entire ai path that is stored
+//    /// </summary>
+//    public class AiPathNode
+//    {
+//        /// <summary>
+//        /// Position of the object
+//        /// </summary>
+//        [XmlElement(nameof(Position))]
+//        public SerializableVector3 Position { get; set; }
+
+//        /// <summary>
+//        /// Rotation of the object expressed as a Quaternion
+//        /// </summary>
+//        [XmlElement(nameof(Rotation))]
+//        public SerializableQuaternion Rotation { get; set; }
+
+//        /// <summary>
+//        /// UpVector of the Object
+//        /// </summary>
+//        [XmlElement(nameof(UpVector))]
+//        public SerializableVector3 UpVector { get; set; }
+
+//        /// <summary>
+//        /// ForwardVector of the Object
+//        /// </summary>
+//        [XmlElement(nameof(ForwardVector))]
+//        public SerializableVector3 ForwardVector { get; set; }
+
+
+//        /// <summary>
+//        /// Speed of the object 
+//        /// </summary>
+//        [XmlElement(nameof(Speed))]
+//        public float Speed { get; set; }
+
+//        /// <summary>
+//        /// Delta time of the movement frame
+//        /// </summary>
+//        [XmlElement(nameof(DeltaTime))]
+//        public float DeltaTime { get; set; }
+//    }
+
+//    /// <summary>
+//    /// Serializable version of the Vector3
+//    /// </summary>
+//    public class SerializableVector3
+//    {
+//        /// <summary>
+//        /// X vector component
+//        /// </summary>
+//        [XmlElement(nameof(X))]
+//        public float X { get; set; }
+
+//        /// <summary>
+//        /// Y vector component
+//        /// </summary>
+//        [XmlElement(nameof(Y))]
+//        public float Y { get; set; }
+
+//        /// <summary>
+//        /// Z vector component
+//        /// </summary>
+//        [XmlElement(nameof(Z))]
+//        public float Z { get; set; }
+
+//        /// <summary>
+//        /// Serializable vector with all coordinates set to zero
+//        /// </summary>
+//        public static SerializableVector3 Zero => new SerializableVector3(0,0,0);
+
+//        public SerializableVector3() { }
+
+//        public SerializableVector3(float x, float y, float z)
+//        {
+//            X = x;
+//            Y = y;
+//            Z = z;
+//        }
+
+//        /// <summary>
+//        /// Returns the string representation of the SerializableVector3
+//        /// </summary>
+//        public override string ToString()
+//        {
+//            return $"({X}, {Y}, {Z})";
+//        }
+//    }
+
+//    /// <summary>
+//    /// Serializable version of the Quaternion
+//    /// </summary>
+//    public class SerializableQuaternion
+//    {
+//        /// <summary>
+//        /// X quaternion component
+//        /// </summary>
+//        [XmlElement(nameof(X))]
+//        public float X { get; set; }
+
+//        /// <summary>
+//        /// Y quaternion component
+//        /// </summary>
+//        [XmlElement(nameof(Y))]
+//        public float Y { get; set; }
+
+//        /// <summary>
+//        /// Z quaternion component
+//        /// </summary>
+//        [XmlElement(nameof(Z))]
+//        public float Z { get; set; }
+
+//        /// <summary>
+//        /// W quaternion component
+//        /// </summary>
+//        [XmlElement(nameof(W))]
+//        public float W { get; set; }
+
+//        /// <summary>
+//        /// Serializable vector with all coordinates set to zero
+//        /// </summary>
+//        public static SerializableQuaternion Identity => new SerializableQuaternion(0, 0, 0, 0);
+
+//        public SerializableQuaternion() { }
+
+//        public SerializableQuaternion(float x, float y, float z, float w)
+//        {
+//            X = x;
+//            Y = y;
+//            Z = z;
+//            W = w;
+//        }
+
+//        /// <summary>
+//        /// Returns the string representation of the SerializableQuaternion
+//        /// </summary>
+//        public override string ToString()
+//        {
+//            return $"({X}, {Y}, {Z}, {W})";
+//        }
+//    }
+
+
+//}
+
--- New Unity Project/Assets/Scripts/AiPathSerializer.cs
+++ New Unity Project/Assets/Scripts/AiPathSerializer.cs
@@ -1,235 +1,235 @@
-﻿using Helpers;
-using Networking;
-using Player;
-using SteroidsSharedData.Serialization;
-using System.Collections.Generic;
-using System.IO;
-using System.Xml;
-using System.Xml.Serialization;
-using UnityEngine;
-using static SteroidsSharedData.Helpers.Constants;
-using System;
+﻿//using Helpers;
+//using Networking;
+//using Player;
+//using SteroidsSharedData.Serialization;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Xml;
+//using System.Xml.Serialization;
+//using UnityEngine;
+//using static SteroidsSharedData.Helpers.Constants;
+//using System;
 
-namespace Serialization
-{
-    /// <summary>
-    /// Should be attached on the object in the scene that needs to have its path created
-    /// Serializes all the data to a file which can be then later transferred to the server
-    /// </summary>
-    public class AiPathSerializer : MonoBehaviour
-    {
-        private static readonly string aiPathDirectoryPath = Constants.Serialization.AiPathAbsoluteDirectoryPath;
-        private static readonly string aiPathFilePath = Constants.Serialization.AiPathAbsoluteFilePath;
-        private static readonly float serializationFrequency = Constants.AiMovementParameters.SerializationFrequency;
+//namespace Serialization
+//{
+//    /// <summary>
+//    /// Should be attached on the object in the scene that needs to have its path created
+//    /// Serializes all the data to a file which can be then later transferred to the server
+//    /// </summary>
+//    public class AiPathSerializer : MonoBehaviour
+//    {
+//        private static readonly string aiPathDirectoryPath = Constants.Serialization.AiPathAbsoluteDirectoryPath;
+//        private static readonly string aiPathFilePath = Constants.Serialization.AiPathAbsoluteFilePath;
+//        private static readonly float serializationFrequency = Constants.AiMovementParameters.SerializationFrequency;
 
-        private AvailableLevels level;
-        private AiPath aiPath;
-        private List<AiPathNode> pathNodes = new List<AiPathNode>();
-        private int bookedLaps;
-        private float elapsedTime;
+//        private AvailableLevels level;
+//        private AiPath aiPath;
+//        private List<AiPathNode> pathNodes = new List<AiPathNode>();
+//        private int bookedLaps;
+//        private float elapsedTime;
 
-        private double totalTime;
-        private double bestTime;
-        private int startPositionIndex;
+//        private double totalTime;
+//        private double bestTime;
+//        private int startPositionIndex;
 
-        private bool shouldWrite;
-        LocalPlayer localPlayer;
-        private int frameCounter = 0;
-        private int framesToSkip = 5;
-        private float frames = 0.0f;
+//        private bool shouldWrite;
+//        LocalPlayer localPlayer;
+//        private int frameCounter = 0;
+//        private int framesToSkip = 5;
+//        private float frames = 0.0f;
 
-        private bool isStartedRecordingOnce = false;
+//        private bool isStartedRecordingOnce = false;
 
-        //public BikeRecorder bikeRecorder;
+//        //public BikeRecorder bikeRecorder;
 
-        private int lapTimeSeconds = 0;
-
-
-        private void Start()
-        {
-            localPlayer = LocalPlayer.Instance;
-            ServerMessageReader.LoadLevelReceived += (e) => { level = e; };
-            ServerMessageReader.ShowLevelReceived += () => { bookedLaps = LocalPlayer.Instance.bookedLaps; };
-            //FinishLine.PlayerCrossedFinishLine += RegisterFinishTime;
-            ServerMessageReader.RaceTimedOut += StopCreatingPath;
-            frameCounter = framesToSkip;
-
-        }
-
-        public void RegisterFinishTime(TimeSpan _totalTime, TimeSpan _bestTime)
-        {
-            //finishIndex = pathNodes.Count - 1;
-            totalTime = _totalTime.TotalMilliseconds;
-            bestTime = _bestTime.TotalMilliseconds;
-        }
-
-        public void RegisterStartPosition(int index)
-        {
-            startPositionIndex = index;
-            switch (level)
-            {
-                case AvailableLevels.DefaultCity:
-                    lapTimeSeconds = (int) LevelLapTimes.SteroidsCity;
-                    break;
-                case AvailableLevels.Dune:
-                    lapTimeSeconds = (int)LevelLapTimes.Dune;
-                    break;
-                default:
-                    break;
-            }
-                    Debug.Log("lap duration set to: " + lapTimeSeconds);
-        }
-
-        //private void Update()
-        //{
-        //    shouldWrite = localPlayer.isRaceStarted;
-
-        //    if (!shouldWrite)
-        //        return;
-
-        //    if(!isStartedRecordingOnce)
-        //    {
-        //        InvokeRepeating("Recording", 0, 0.1f);
-        //        isStartedRecordingOnce = true;
-        //    }
-
-        //    //elapsedTime += Time.deltaTime;
-        //    //if (elapsedTime < serializationFrequency)
-        //    //    return;
-
-        //    //elapsedTime = 0;
-        //    //Vector3 position = transform.position;
-        //    //Quaternion rotation = transform.rotation;
-        //    //Vector3 upVector = transform.up;
-        //    //Vector3 forwardVector = transform.forward;
-
-        //    //AiPathNode node = new AiPathNode()
-        //    //{
-        //    //    Position = new SerializableVector3(position.x, position.y, position.z),
-        //    //    Rotation = new SerializableQuaternion(rotation.x, rotation.y, rotation.z, rotation.w),
-        //    //    UpVector = new SerializableVector3(upVector.x, upVector.y, upVector.z),
-        //    //    ForwardVector = new SerializableVector3(forwardVector.x, forwardVector.y, forwardVector.z),
-        //    //    Speed = LocalPlayer.Instance.MoveSpeed,
-        //    //    DeltaTime = serializationFrequency
-        //    //};
-        //    //pathNodes.Add(node);
-        //}
-
-        private void FixedUpdate()
-        {
-            //shouldWrite = localPlayer.isRaceStarted;
-
-            if (!shouldWrite)
-                return;
-
-            frameCounter--;
-            if (frameCounter <= 0)
-            {
-                WriteNode();
-            }
-
-        }
+//        private int lapTimeSeconds = 0;
 
 
-        public void StartRecording()
-        {
-            shouldWrite = true;
-            //bikeRecorder.enabled = true;
-        }
+//        private void Start()
+//        {
+//            localPlayer = LocalPlayer.Instance;
+//            ServerMessageReader.LoadLevelReceived += (e) => { level = e; };
+//            ServerMessageReader.ShowLevelReceived += () => { bookedLaps = LocalPlayer.Instance.bookedLaps; };
+//            //FinishLine.PlayerCrossedFinishLine += RegisterFinishTime;
+//            ServerMessageReader.RaceTimedOut += StopCreatingPath;
+//            frameCounter = framesToSkip;
 
-        public void WriteNode()
-        {
+//        }
 
-            if (localPlayer.bookedLaps * lapTimeSeconds * 10 + 1 <= pathNodes.Count)
-            {
-                return;
-            }
+//        public void RegisterFinishTime(TimeSpan _totalTime, TimeSpan _bestTime)
+//        {
+//            //finishIndex = pathNodes.Count - 1;
+//            totalTime = _totalTime.TotalMilliseconds;
+//            bestTime = _bestTime.TotalMilliseconds;
+//        }
 
-            Vector3 position = transform.position;
-            Quaternion rotation = transform.rotation;
-            Vector3 upVector = transform.up;
-            Vector3 forwardVector = transform.forward;
+//        public void RegisterStartPosition(int index)
+//        {
+//            startPositionIndex = index;
+//            switch (level)
+//            {
+//                case AvailableLevels.DefaultCity:
+//                    lapTimeSeconds = (int) LevelLapTimes.SteroidsCity;
+//                    break;
+//                case AvailableLevels.Dune:
+//                    lapTimeSeconds = (int)LevelLapTimes.Dune;
+//                    break;
+//                default:
+//                    break;
+//            }
+//                    Debug.Log("lap duration set to: " + lapTimeSeconds);
+//        }
 
-            AiPathNode node = new AiPathNode()
-            {
-                Position = new SerializableVector3(position.x, position.y, position.z),
-                Rotation = new SerializableQuaternion(rotation.x, rotation.y, rotation.z, rotation.w),
-                UpVector = new SerializableVector3(upVector.x, upVector.y, upVector.z),
-                ForwardVector = new SerializableVector3(forwardVector.x, forwardVector.y, forwardVector.z),
-                Speed = LocalPlayer.Instance.MoveSpeed,
-                DeltaTime = serializationFrequency
-            };
-            pathNodes.Add(node);
-            frameCounter = framesToSkip;
-        }
+//        //private void Update()
+//        //{
+//        //    shouldWrite = localPlayer.isRaceStarted;
+
+//        //    if (!shouldWrite)
+//        //        return;
+
+//        //    if(!isStartedRecordingOnce)
+//        //    {
+//        //        InvokeRepeating("Recording", 0, 0.1f);
+//        //        isStartedRecordingOnce = true;
+//        //    }
+
+//        //    //elapsedTime += Time.deltaTime;
+//        //    //if (elapsedTime < serializationFrequency)
+//        //    //    return;
+
+//        //    //elapsedTime = 0;
+//        //    //Vector3 position = transform.position;
+//        //    //Quaternion rotation = transform.rotation;
+//        //    //Vector3 upVector = transform.up;
+//        //    //Vector3 forwardVector = transform.forward;
+
+//        //    //AiPathNode node = new AiPathNode()
+//        //    //{
+//        //    //    Position = new SerializableVector3(position.x, position.y, position.z),
+//        //    //    Rotation = new SerializableQuaternion(rotation.x, rotation.y, rotation.z, rotation.w),
+//        //    //    UpVector = new SerializableVector3(upVector.x, upVector.y, upVector.z),
+//        //    //    ForwardVector = new SerializableVector3(forwardVector.x, forwardVector.y, forwardVector.z),
+//        //    //    Speed = LocalPlayer.Instance.MoveSpeed,
+//        //    //    DeltaTime = serializationFrequency
+//        //    //};
+//        //    //pathNodes.Add(node);
+//        //}
+
+//        private void FixedUpdate()
+//        {
+//            //shouldWrite = localPlayer.isRaceStarted;
+
+//            if (!shouldWrite)
+//                return;
+
+//            frameCounter--;
+//            if (frameCounter <= 0)
+//            {
+//                WriteNode();
+//            }
+
+//        }
 
 
-        private void StopCreatingPath()
-        {
-            Debug.Log("framecounter: " + frameCounter);
-            Debug.Log("StopRecord Write: " + Time.time + " nodes: " + pathNodes.Count);
-            WriteNode();
-            //CancelInvoke("Recording");
-            shouldWrite = false;
-            //bikeRecorder.enabled = false;
-            elapsedTime = 0;
+//        public void StartRecording()
+//        {
+//            shouldWrite = true;
+//            //bikeRecorder.enabled = true;
+//        }
 
-            FillDataForSerialization();
-            pathNodes.Clear();
+//        public void WriteNode()
+//        {
 
-            EnsureFileExistance();
-            SerializeAiPath();
-        }
+//            if (localPlayer.bookedLaps * lapTimeSeconds * 10 + 1 <= pathNodes.Count)
+//            {
+//                return;
+//            }
 
-        void Recording()
-        {
-            Vector3 position = transform.position;
-            Quaternion rotation = transform.rotation;
-            Vector3 upVector = transform.up;
-            Vector3 forwardVector = transform.forward;
+//            Vector3 position = transform.position;
+//            Quaternion rotation = transform.rotation;
+//            Vector3 upVector = transform.up;
+//            Vector3 forwardVector = transform.forward;
 
-            AiPathNode node = new AiPathNode()
-            {
-                Position = new SerializableVector3(position.x, position.y, position.z),
-                Rotation = new SerializableQuaternion(rotation.x, rotation.y, rotation.z, rotation.w),
-                UpVector = new SerializableVector3(upVector.x, upVector.y, upVector.z),
-                ForwardVector = new SerializableVector3(forwardVector.x, forwardVector.y, forwardVector.z),
-                Speed = LocalPlayer.Instance.MoveSpeed,
-                DeltaTime = serializationFrequency
-            };
-            pathNodes.Add(node);
-        }
+//            AiPathNode node = new AiPathNode()
+//            {
+//                Position = new SerializableVector3(position.x, position.y, position.z),
+//                Rotation = new SerializableQuaternion(rotation.x, rotation.y, rotation.z, rotation.w),
+//                UpVector = new SerializableVector3(upVector.x, upVector.y, upVector.z),
+//                ForwardVector = new SerializableVector3(forwardVector.x, forwardVector.y, forwardVector.z),
+//                Speed = LocalPlayer.Instance.MoveSpeed,
+//                DeltaTime = serializationFrequency
+//            };
+//            pathNodes.Add(node);
+//            frameCounter = framesToSkip;
+//        }
 
-        private void EnsureFileExistance()
-        {
-            if (!Directory.Exists(aiPathDirectoryPath))
-            {
-                Directory.CreateDirectory(aiPathDirectoryPath);
-                File.Create(aiPathFilePath).Dispose();
-            }
-            else if (!File.Exists(aiPathFilePath))
-            {
-                File.Create(aiPathFilePath.AppendTimeStamp()).Dispose();
-            }
-        }
 
-        private void FillDataForSerialization()
-        {
-            AiPathNode[] nodesToStore = new AiPathNode[pathNodes.Count];
-            for (int i = 0; i < nodesToStore.Length; ++i)
-                nodesToStore[i] = pathNodes[i];
+//        private void StopCreatingPath()
+//        {
+//            Debug.Log("framecounter: " + frameCounter);
+//            Debug.Log("StopRecord Write: " + Time.time + " nodes: " + pathNodes.Count);
+//            WriteNode();
+//            //CancelInvoke("Recording");
+//            shouldWrite = false;
+//            //bikeRecorder.enabled = false;
+//            elapsedTime = 0;
 
-            aiPath = new AiPath() { PathNodeCollection = nodesToStore, LevelName = level, StartPositionIndex = startPositionIndex, BookedLaps = bookedLaps, TotalTime = totalTime, BestTime = bestTime };
-        }
+//            FillDataForSerialization();
+//            pathNodes.Clear();
 
-        private void SerializeAiPath()
-        {
-            var serializer = new XmlSerializer(typeof(AiPath));
-            using (var writer = XmlWriter.Create(aiPathFilePath))
-            {
-                serializer.Serialize(writer, aiPath);
-            }
-        }
+//            EnsureFileExistance();
+//            SerializeAiPath();
+//        }
 
-    }
-}
+//        void Recording()
+//        {
+//            Vector3 position = transform.position;
+//            Quaternion rotation = transform.rotation;
+//            Vector3 upVector = transform.up;
+//            Vector3 forwardVector = transform.forward;
+
+//            AiPathNode node = new AiPathNode()
+//            {
+//                Position = new SerializableVector3(position.x, position.y, position.z),
+//                Rotation = new SerializableQuaternion(rotation.x, rotation.y, rotation.z, rotation.w),
+//                UpVector = new SerializableVector3(upVector.x, upVector.y, upVector.z),
+//                ForwardVector = new SerializableVector3(forwardVector.x, forwardVector.y, forwardVector.z),
+//                Speed = LocalPlayer.Instance.MoveSpeed,
+//                DeltaTime = serializationFrequency
+//            };
+//            pathNodes.Add(node);
+//        }
+
+//        private void EnsureFileExistance()
+//        {
+//            if (!Directory.Exists(aiPathDirectoryPath))
+//            {
+//                Directory.CreateDirectory(aiPathDirectoryPath);
+//                File.Create(aiPathFilePath).Dispose();
+//            }
+//            else if (!File.Exists(aiPathFilePath))
+//            {
+//                File.Create(aiPathFilePath.AppendTimeStamp()).Dispose();
+//            }
+//        }
+
+//        private void FillDataForSerialization()
+//        {
+//            AiPathNode[] nodesToStore = new AiPathNode[pathNodes.Count];
+//            for (int i = 0; i < nodesToStore.Length; ++i)
+//                nodesToStore[i] = pathNodes[i];
+
+//            aiPath = new AiPath() { PathNodeCollection = nodesToStore, LevelName = level, StartPositionIndex = startPositionIndex, BookedLaps = bookedLaps, TotalTime = totalTime, BestTime = bestTime };
+//        }
+
+//        private void SerializeAiPath()
+//        {
+//            var serializer = new XmlSerializer(typeof(AiPath));
+//            using (var writer = XmlWriter.Create(aiPathFilePath))
+//            {
+//                serializer.Serialize(writer, aiPath);
+//            }
+//        }
+
+//    }
+//}
