02d1e3399b7616a1577e1818c6c8f472d549856a
Daniel Jazz Young <danny.young@icaros.com>
2019-09-27 17:06:26 +0200

    +++Finished replay dummy, working on animation conversion. Only that and Bend goal implementation left!

---
+++ New Unity Project/Assets/Scripts/AnimationConverter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AnimationConverter : EditorWindow
+{
+    [SerializeField]
+    private string animationName;
+    [SerializeField]
+    private TextAsset textAsset;
+
+   [MenuItem("Window/Animation Converter")]
+
+   static void animationConverter()
+    {
+        EditorWindow myWindow = EditorWindow.GetWindow<AnimationConverter>();
+        myWindow.minSize = new Vector2(512, 256);
+        myWindow.maxSize = myWindow.minSize;
+    }
+
+
+    private void OnGUI()
+    {
+        EditorGUILayout.Space();
+        GUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Select name for animation.");
+        animationName = EditorGUILayout.TextField(animationName);
+        GUILayout.EndHorizontal();
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        GUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Choose Textasset to convert.");
+        textAsset = (TextAsset)EditorGUILayout.ObjectField(textAsset, typeof(TextAsset));
+
+        if (GUILayout.Button("Convert!"))
+        {
+
+        }
+        
+
+
+
+    }
+
+    void ExtractXMLData()
+    {
+
+    }
+
+    void ConvertData()
+    {
+
+    }
+}
