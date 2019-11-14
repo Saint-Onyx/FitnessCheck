33bb65969d6e8841fa3b89370c2171a13efb3b9a
Daniel Jazz Young <daniel-young@web.de>
2019-11-14 10:09:12 +0100

    +++Renamed Unity Project

---
+++ Fitness Application/Assets/Scripts/QoLChanges.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QoLChanges : MonoBehaviour
+{
+    public MeshRenderer dumbbellLeft;
+    public MeshRenderer dumbbellRight;
+    public Animator animator;
+
+
+    public void enableDumbbell()
+    {
+        if (dumbbellLeft && dumbbellRight)
+        {
+            dumbbellLeft.enabled = true;
+            dumbbellRight.enabled = true;
+        }
+    }
+
+
+    public void disableDumbbell()
+    {
+        if (dumbbellLeft && dumbbellRight)
+        {
+            dumbbellLeft.enabled = false;
+            dumbbellRight.enabled = false;
+        }
+    }
+
+    /// <summary>
+    /// Allow the user to change animation states with the UI buttons.
+    /// </summary>
+    /// <param name="id"></param>
+    public void changeAnimation(int id)
+    {
+        switch (id)
+        {
+            case 1:
+                animator.SetTrigger("DBC");
+                break;
+            case 2:
+                animator.SetTrigger("SDP");
+                break;
+            case 3:
+                animator.SetTrigger("OLS");
+                break;
+            case 4:
+                animator.SetTrigger("DBR");
+                break;
+            default:
+                break;
+        }
+    }
+}
